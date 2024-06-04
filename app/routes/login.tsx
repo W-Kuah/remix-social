e63c97c10201099b9e2f99ec1137b0623f136d22
Warren Kuah <warrenkuah@Warrens-MacBook-Air.local>
2024-06-05 00:32:32 +1000

    google oauth

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -5,8 +5,10 @@ import { AppLogo } from "~/components/app-logo";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent } from "~/components/ui/card";
 import { Github } from "lucide-react";
+import { User } from "lucide-react";
 import { SupabaseOutletContext } from "~/lib/supabase";
 import { getSupabaseWithSessionAndHeaders } from "~/lib/supabase.server";
+import { Provider } from "@supabase/supabase-js";
 
 export let loader = async ({ request }: LoaderFunctionArgs) => {
     const { headers, serverSession } = await getSupabaseWithSessionAndHeaders({
@@ -23,9 +25,9 @@ export let loader = async ({ request }: LoaderFunctionArgs) => {
 export default function Login() {
     const { supabase, domainUrl } = useOutletContext<SupabaseOutletContext>();
 
-    const handleSignIn = async () => {
+    const handleSignIn = async (provider: Provider) => {
         await supabase.auth.signInWithOAuth({
-            provider: "github",
+            provider: provider,
             options: {
                 redirectTo: `${domainUrl}/resources/auth/callback`,
             },
@@ -45,8 +47,12 @@ export default function Login() {
             <div className="flex flex-col items-center space-y-4 text-center p-4">
             <h1 className="text-3xl md:text-5xl font-bold tracking-tighter">
                 Login in using <br />
-                <span className="px-1 font-extrabold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">
+                <span className="px-1 font-extrabold bg-gradient-to-r from-orange-700 via-green-500 to-purple-400 text-transparent bg-clip-text bg-300% animate-gradient">
                 Github
+                </span>{" "}
+                or
+                {" "}<span className="px-1 font-extrabold bg-gradient-to-r from-blue-400 via-red-700 to-yellow-600 text-transparent bg-clip-text bg-300% animate-gradient">
+                Google
                 </span>{" "}
                 <br />
                 and discover more
@@ -57,10 +63,18 @@ export default function Login() {
             </p>
             </div>
             <Card className="relative group overflow-hidden rounded-lg">
-            <CardContent className="p-1 bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 bg-300% animate-gradient">
-                <Button onClick={ handleSignIn }>
+            <CardContent className="p-1 bg-gradient-to-r from-orange-700 via-purple-500 to-green-400  bg-300% animate-gradient">
+                <Button onClick={() => handleSignIn('github' as Provider) }>
                     <Github className="mr-2 h-4 w-4"/>
                         Github
+                </Button>
+            </CardContent>
+            </Card>
+            <Card className="relative group overflow-hidden rounded-lg">
+            <CardContent className="p-1 bg-gradient-to-r from-blue-400 via-red-700 to-yellow-600 bg-300% animate-gradient">
+                <Button onClick={() => handleSignIn('google' as Provider) }>
+                    <User className="mr-2 h-4 w-4"/>
+                        Google
                 </Button>
             </CardContent>
             </Card>
